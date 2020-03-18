d4c4e4607bb2046ae543cda41279583ad9fe209d
Sven Stahl <vagrant@CompilerLab.box>
2020-03-18 14:18:35 +0000

    makes symbol table available in other files

--- src/symbol_table.h
+++ src/symbol_table.h
@@ -1,26 +1,38 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+// enum for different symbol types
 enum symbol_type 
 {
     INT, ARRAY, FUNC
 };
 
+// enum for different function return types
 enum func_return_type
 {
     INT, VOID
 };
 
-struct symbol_table
+// id, typ (int, array, func), 
+// func: return type, num param
+// array: length
+typedef struct symbol_table_struct
 {
 
-    char* id;
-    symbol_type type;
+    // general variables for all symbols:
+    char* id; // name of the element
+    symbol_type type; // type of the symbol
+    symbol_table_struct *next; // next element in the symbol table 
 
-    func_return_type return_type;
-    unsigned int param_count;
+    // variables for functions:
+    func_return_type return_type; // return type of the function 
+    unsigned int param_count; // parameter count of the function
 
-    unsigned int length;
+    // variables for arrays:
+    unsigned int length; // array length
 
-    // id, typ (int, array, func), 
-    // func: return type, num param
-    // array: length
+} symbol_table_type;
 
-};
+extern symbol_table_type symbol_table;
+
+#endif
