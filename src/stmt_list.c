a9d0169384beeb0fb67745af8303c5e0c1131e0f
vagrant <vagrant@CompilerLab.box>
2020-04-26 14:32:09 +0000

    publish data structures

--- src/stmt_list.c
+++ src/stmt_list.c
@@ -2,13 +2,48 @@
 
 stmt_list_element* stmt_from_expr(value* expr)
 {
-    stmt_list_element* new_element = malloc(sizeof(stmt_list_element));
-    new_element->next = NULL;
-    new_element->scope = numberOfScopes;
-    new_element->type = STMT_TYPE_EXPR;
-    new_element->stmt.expr = expr;
+    if(expr->next_expr == NULL)
+    {
+        stmt_list_element* new_element = malloc(sizeof(stmt_list_element));
+        new_element->next = NULL;
+        new_element->scope = numberOfScopes;
+        new_element->type = STMT_TYPE_EXPR;
+        new_element->stmt.expr = expr;
 
-    return new_element;
+        return new_element;
+    }
+    
+    value* current_value = expr;
+    int numberOfTemps = 0;
+    while(true)
+    {
+        if(current_value->next_expr != NULL)
+        {
+            if(current_value->next_expr->next_expr != NULL)
+            {
+
+            }
+            else // no more expressions 
+            {
+                // create statement for temp variable
+                stmt_list_element* tmp_stmt = malloc(sizeof(stmt_list_element));
+                tmp_stmt->next = NULL;
+                tmp_stmt->scope = numberOfScopes;
+                tmp_stmt->type = STMT_TYPE_EXPR;
+                strcpy(tmp_stmt->stmt.stmt_expr->dest, strcat("temp_", itoa(numberOfTemps)));
+                strcpy(tmp_stmt->stmt.stmt_expr->op, current_value->stmt_operator);
+                if(current_value->valueType == VALUE_TYPE_VALUE)
+                {
+                    strcpy(tmp_stmt->stmt.stmt_expr->operand1, itoa(current_value->value.rval));
+                }
+
+                if(current_value->next_expr->valueType == VALUE_TYPE_VALUE)
+                {
+                    strcpy(tmp_stmt->stmt.stmt_expr->operand2, itoa(current_value->next_expr->value.rval));
+                }
+            }
+        }
+    }
 }
 
 stmt_list_element* stmt_from_cond(value* cond_expr, stmt_list_element* true_list, stmt_list_element* false_list)
@@ -25,4 +60,46 @@ stmt_list_element* stmt_from_cond(value* cond_expr, stmt_list_element* true_list
     }
 
     return new_element;
+}
+
+stmt_list_element* stmt_from_loop(value* cond_expr, stmt_list_element* loop_list, bool doWhile)
+{
+    stmt_list_element* new_element = malloc(sizeof(stmt_list_element));
+    new_element->next = NULL;
+    new_element->scope = numberOfScopes;
+    new_element->type = STMT_TYPE_LOOP;
+    new_element->stmt.stmt_loop->cond_expr = cond_expr;
+    new_element->stmt.stmt_loop->loop_list = loop_list;
+    new_element->stmt.stmt_loop->do_while = doWhile;
+
+    return new_element;
+}
+
+stmt_list_element* stmt_from_return(value* expr)
+{
+    stmt_list_element* new_element = malloc(sizeof(stmt_list_element));
+    new_element->next = NULL;
+    new_element->scope = numberOfScopes;
+    new_element->type = STMT_TYPE_RETURN;
+    if(expr != NULL)
+    {
+        new_element->stmt.stmt_return->return_expr = expr;
+    }
+    else
+    {
+        new_element->stmt.stmt_return->return_expr = NULL;
+    }
+
+
+    return new_element;
+}
+
+void set_expr_details (char* op, value* currentExpr, value* nextExpr)
+{
+    if(op != NULL)
+    {
+        currentExpr->stmt_operator = op;
+    }
+
+    currentExpr->next_expr = nextExpr;
 }
--- src/stmt_list.h
+++ src/stmt_list.h
@@ -1,23 +1,10 @@
 #ifndef STMT_LIST_H
 #define STMT_LIST_H
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "type_checks.h"
-
-typedef struct stmt_list_struct
-{
-    stmt_list_element* next;
-    stmt_type type;
-    stmt stmt;
-    int scope;
-} stmt_list_element;
-
-typedef union stmt_union
-{
-    value* expr;
-    stmt_cond* stmt_cond;
-    stmt_loop* stmt_loop;
-    stmt_return* stmt_return;
-} stmt;
 
 typedef enum stmt_enum 
 {
@@ -46,6 +33,39 @@ typedef struct stmt_return_struct
 {
     value* return_expr;
 } stmt_return;
+
+typedef struct stmt_expr_struct
+{
+    char* dest;
+    char* op;
+    char* operand1;
+    char* operand2;
+} stmt_expr;
+
+
+typedef struct stmt_list_struct
+{
+    stmt_list_element* next;
+    stmt_type type;
+    stmt stmt;
+    int scope;
+} stmt_list_element;
+
+typedef union stmt_union
+{
+    value* expr;
+    stmt_expr* stmt_expr;
+    stmt_cond* stmt_cond;
+    stmt_loop* stmt_loop;
+    stmt_return* stmt_return;
+} stmt;
+
+
+extern stmt_list_element* stmt_from_expr(value* expr);
+extern stmt_list_element* stmt_from_cond(value* cond_expr, stmt_list_element* true_list, stmt_list_element* false_list);
+extern stmt_list_element* stmt_from_loop(value* cond_expr, stmt_list_element* loop_list, bool doWhile);
+extern stmt_list_element* stmt_from_return(value* expr);
+extern void set_expr_details(char* op, value* currentExpr, value* nextExpr);
 
 
 
