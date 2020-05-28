96c22dbcf4e029a8f52b465f0b7e0e192814f7b5
Sven Stahl <vagrant@CompilerLab.box>
2020-05-28 16:55:33 +0000

    Small fix

--- src/stmt_list.c
+++ src/stmt_list.c
@@ -740,6 +740,8 @@ void print_stmt_expr (FILE *fp, stmt_expr* stmt_expr)
     // print function call
     if (!strcmp(stmt_expr->op, "func"))
         fprintf (fp, "%s;\n", stmt_expr->dest);
+    else 
+    {
 
     if (strcmp(stmt_expr->operand2, "") && strcmp(stmt_expr->op, "") && strcmp(stmt_expr->op, "="))
         fprintf (fp, "%s = %s %s %s;\n", stmt_expr->dest, stmt_expr->operand1, stmt_expr->op, stmt_expr->operand2);
@@ -749,6 +751,8 @@ void print_stmt_expr (FILE *fp, stmt_expr* stmt_expr)
 
     if (!strcmp(stmt_expr->operand2, "") && strcmp(stmt_expr->op, "") && !strcmp(stmt_expr->op, "="))
         fprintf (fp, "%s = %s;\n", stmt_expr->dest, stmt_expr->operand1);
+        
+    }
 
 }
 
